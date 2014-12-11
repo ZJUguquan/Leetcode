e99b343a7fe2e32cc3f3ff7fe3c0277682bd3c65
staticor <staticor0828@gmail.com>
2014-12-11 14:14:21 +0800

    add a cw. epher code.

---
+++ codewars/polybius.py
@@ -0,0 +1,16 @@
+letter2num = {
+    'A':11,'B':12,'C':13,'D':14,'E':15,
+    'F':21,'G':22,'H':23,'I':24,'J':24,'K':25,
+    'L':31,'M':32,'N':33,'O':34,'P':35,
+    'Q':41,'R':42,'S':43,'T':44,'U':45,
+    'V':51,'W':52,'X':53,'Y':54,'Z':55,
+}
+
+def res(l):
+    if l in letter2num:
+        return str(letter2num[l])
+    else:
+        return l
+
+def polybius(s):
+    return ''.join([ res(letter) for letter in s] )
