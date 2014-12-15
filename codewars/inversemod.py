945ccf904bd98fd546abce5b9864da977440b7d3
staticor <staticor0828@gmail.com>
2014-12-15 17:17:01 +0800

    a cw, similar with leetcode, romman code.

--- codewars/inversemod.py
+++ codewars/inversemod.py
@@ -1,14 +1,18 @@
 def inverseMod(a, m):
-    '''
-    test.assert_equals(inverseMod(2,5), 3)
-    test.assert_equals(inverseMod(48,101), 40)
-    '''
     for i in range(1,m):
         if ( m*i + 1) % a == 0:
             return ( m*i + 1) // a
     return None
 
 
-print(inverseMod(2,5))
-print(inverseMod(48,101))
-print(inverseMod(2, 40))
+# print(inverseMod(2,5))
+# print(inverseMod(48,101))
+# print(inverseMod(2, 40))
+
+
+
+from unittest import TestCase
+test =TestCase()
+
+test.assertEqual(inverseMod(2,5), 3)
+test.assertEqual(inverseMod(48,99999997), 40)
