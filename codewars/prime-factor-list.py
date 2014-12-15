ccf2336026e8b23003c8977b5124ae2cc56e04a8
staticor <staticor0828@gmail.com>
2014-12-15 13:40:15 +0800

    a cw. factor - list.

--- codewars/prime-factor-list.py
+++ codewars/prime-factor-list.py
@@ -1,22 +1,27 @@
 
-def is_prime(n):
-    '''
-    check it is prime number
-    '''
-    return n > 1 and all(n % i for i in range(2, n))
+# def is_prime(n):
+#     '''
+#     check it is prime number
+#     '''
+#     return n > 1 and all(n % i for i in range(2, n))
 
 
 def prime_factors(n):
-    res = []
-    if is_prime(n):
-        return [n]
-    factor_list = [i for i in range(int(n//2), 1, -1) if is_prime(i)]
-    curr = 0
-    while n > 1:
-        while n % factor_list[curr] == 0:
-            n = n // factor_list[curr]
-            res.append(factor_list[curr])
-        curr += 1
-    return res[::-1]
+    from math import sqrt
+    f, res =3, []
+    # if is_prime(n):
+    #     return [n]
+    while n % 2 == 0:
+        res.append(2)
+        n //= 2
 
-print(prime_factors(1001))
+    while f * f <= n:
+        while n % f == 0:
+            res.append(f)
+            n //= f
+        f += 2
+    if n > 1:
+        res.append(n)
+    return res
+
+print(prime_factors(101))
