a05ebc9d713e16eb09533bfa7d2816adb209d8a2
staticor <staticor0828@gmail.com>
2014-12-23 13:40:13 +0800

    century. solu.

--- codewars/whatcentury.py
+++ codewars/whatcentury.py
@@ -1,10 +1,20 @@
+'solu'
 def whatCentury(year):
-    tail = {'1':'st', '2':'nd', '3':'rd'}
-    if year % 100 == 0:
-        return str(year)[:2]+ 'th'
-    else:
-        t = str(year)[2]
-        return str(year+100)[:2] + 'th' if t not in tail else str(year+100)[:2] + tail[t]
+    indicators = {'1': 'st', '2': 'nd', '3': 'rd'}
+    century = str(int(year) // 100 + (1 if int(year) % 100 > 0 else 0))
+    return century + (indicators.get(century[-1], 'th') if century[0] != '1' else 'th')
 
-for i in range(1999, 2305, 9):
+def whatCentury(year):
+    tail = {'1': 'st', '2': 'nd', '3': 'rd'}
+    if year % 100 == 0:
+        return str(year)[:2] + 'th'
+    else:
+        if year < 2000:
+            return str(year + 100)[:2] + 'th'
+        else:
+            t = str(year + 100)[1]
+            return str(year + 100)[:2] + 'th' if t not in tail else str(year + 100)[:2] + tail[t]
+
+l = [1999, 2011, 2154, 2259, 1124, 2000]
+for i in l:
     print(whatCentury(i))
