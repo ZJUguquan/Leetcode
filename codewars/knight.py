af9b0e690f47b74b2e099f340d75366fd0e827f4
staticor <staticor0828@gmail.com>
2015-01-14 15:36:09 +0800

    2 cw.

--- codewars/knight.py
+++ codewars/knight.py
@@ -1,46 +1,52 @@
 'i am a knight'
 
 
-minus = lambda x, y: abs(ord(x) - ord(y))
+def position(p):
+    return ord(p[0]) * 10 + int(p[1])
 
-def one_step_knight_position(p1, p2):
-    x = minus(p1[0], p2[0])
-    y = minus(p1[1], p2[1])
-    if (x,y) in [(2, 1), (1, 2)]:
-        return True
-    return False
-
-def one_step(p1, p2):
-    if abs(p1[0] - p[2[0]]) == 1 and abs(p1[1] - p[2[1]]) == 2:
-        return True
-    if abs(p1[0] - p[2[0]]) == 2 and abs(p1[1] - p[2[1]]) == 1:
-        return True
-    return False
 
 def in_boundary(p):
-    if 1<=p[0]<=8 and 1<=p[1]<=8:
+    if 1 <= p[0] <= 8 and 1 <= p[1] <= 8:
         return True
     return False
 
-def shortest(p1, p2):
 
-    # p1 to p2
-    hori = [-1,-1, 1, 1, -2, -2, 2, 2]
+def next_position(p1):
+    hori = [-1, -1, 1, 1, -2, -2, 2, 2]
     vori = [2, -2, 2, -2, 1, -1, 1, -1]
     x, y = ord(p1[0]) - 96, int(p1[1])
-    x2, y2= ord(p2[0]) - 96, int(p2[1])
-    if one_step((x, y), (x2, y2)):
-        return 1
-
     next_p1_candidate = []
     for i in range(8):
-        next_p1_candidate.append((x+hori[i], y+vori[i]))
-    # print(next_p1_candidate)
+        next_p1_candidate.append((x + hori[i], y + vori[i]))
     next_p1 = [i for i in next_p1_candidate if in_boundary(i)]
-    return min([shortest ])
+    return [chr(96 + i[0]) + str(i[1]) for i in next_p1]
+
+# print(next_position('a1'))
 
 
-p1 = 'a7'
-p2 = 'b3'
+# def one_step(p1, p2):
+#     if abs(p1[0] - p2[0]) == 1 and abs(p1[1] - p2[1]) == 2:
+#         return True
+#     if abs(p1[0] - p2[0]) == 2 and abs(p1[1] - p2[1]) == 1:
+#         return True
+#     return False
+
+
+def shortest(p1, p2):
+    if p1 == p2:
+        return 0
+    next_p1_candidate = next_position(p1)
+    if p2 in next_p1_candidate:
+        return 1
+    used_step = 1
+    while p2 not in next_p1_candidate:
+        for i in next_p1_candidate:
+            next_p1_candidate = next_p1_candidate + next_position(i)
+        used_step += 1
+    return used_step
+
+
+p1 = 'a1'
+p2 = 'f4'
 # print(one_step_knight_position(p1, p2))
 print(shortest(p1, p2))
