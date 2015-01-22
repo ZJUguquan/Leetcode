30554dacb43edea4f188b533b24b30e8acff7926
staticor <staticor0828@gmail.com>
2015-01-22 14:18:35 +0800

    4 ac.

--- codewars/fourPower.py
+++ codewars/fourPower.py
@@ -1,19 +1,16 @@
-def is4m(n):
-    if not isinstance(n, int):
+from math import log
+
+def powerof4(n):
+  return n==4**round(log(n,4)) if (type(n)==int and n>0) else False
+
+
+def powerof4(n):
+    if type(n) != type(1):
         return False
+    if n == 1 :
+            return True
     while n % 4 == 0:
-        n = n // 4
-    if n == 1 or n == -1:
+        n  = n // 4
+    if (n == 1 or n == -1 ):
         return True
     return False
-
-
-print(is4m(-16))
-
-print(1 == True)
-
-print(isinstance(True, int))
-
-n = True
-if n is not True and (n == 1 or n == -1 ):
-        print(32)
