5d9f04583808b5ea58ac1fd5f3e95ba5917ab379
staticor <staticor0828@gmail.com>
2015-02-11 15:44:12 +0800

    mid of sum..

--- codewars/sum-of-pairs.py
+++ codewars/sum-of-pairs.py
@@ -1,13 +1,26 @@
+
 def sum_pairs(list, sum):
-    for idx, elem in enumerate(list):
-        if sum - elem in list and elem != sum//2:
-            return [elem, list[list.index(sum-elem)]]
-    return None
+  s = set()
+  for v in list:
+    if (sum - v) in s:
+      return [sum - v, v]
+    s.add(v)
 
 
-list = [11, 3, 7, 5]
-s = 10
-list =  [10, 5, 2, 3, 7, 5] # [5, 9, 13, -3]
-s = 10
-print(sum_pairs(list, s))
+def sum_pairs(alist, sum):
+    result = []
+    for elem in alist[::-1]:
+        if sum - elem in alist[idx + 1:]:
+            result.append([elem, sum])
+    return result.pop() if len(result) > 0 else None
 
+
+ls = [[1, 4, 8, 7, 3, 15],
+      [1, -2, 3, 0, -6, 1],
+      [20, -13, 40], [1, 2, 3, 4, 1, 0],
+      [10, 5, 2, 3, 7, 5], [4, -2, 3, 3, 4], [0, 2, 0], [5, 9, 13, -3]
+      ]
+sums = [8, -6, -7, 2, 10, 8, 0, 10]
+print('test ... test')
+for idx, tlist in enumerate(ls):
+    print(sum_pairs(tlist, sums[idx]), '---', sums[idx])
