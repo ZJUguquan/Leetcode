ad85ac6bb9fcf832b797cc8df174a699a7a583a3
staticor <staticor0828@gmail.com>
2015-04-28 13:19:43 +0800

    fixed.

--- codewars/is_prime.py
+++ codewars/is_prime.py
@@ -9,3 +9,14 @@ def is_prime(n):
 
 def is_prime(n):
   return n > 1 and all(n % i for i in xrange(2, n))
+
+
+  def number_property(n):
+    if n <= 1:
+        is_prime = False
+    else:
+        is_prime = True
+        for i in xrange(2, int(n ** 0.5) + 1):
+            if (n % i) == 0:
+                is_prime = False
+    return [is_prime, (n%2 == 0), (n%10==0)]
