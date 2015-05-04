55baf1b285ae4e363e8f3686685b057f199fa785
staticor <staticor0828@gmail.com>
2015-05-04 15:24:53 +0800

    square 矩形分割.

--- codewars/square into squares-decomposition.py
+++ codewars/square into squares-decomposition.py
@@ -1,60 +1,88 @@
 # -*- coding: utf-8 -*-
 
-la = """
-Given a positive integral number n, return a strictly increasing sequence (list/array/string depending on the language) of numbers, so that the sum of the squares is equal to n².
 
-If there are multiple solutions (and there will be), return the result with the largest possible values:
+import math
 
-Examples:
-
-decompose(11) must return [1,2,4,10] 11² = 121 = 1 + 4 + 16 + 100 = 1² + 2² + 4² + 10² but don't return [2,6,9], since 9 is smaller than 10.
-
-For decompose(50) don't return [1, 1, 4, 9, 49] but [1, 3, 5, 8, 49] since [1, 1, 4, 9, 49] doen't form a strictly increasing sequence.
-
-Note: Neither [n] nor [1,1,1,…,1] are valid solutions. If no valid solution exists, return nil, null, Nothing, None or "".
-
-The function "decompose" will take a positive integer n and return the decomposition of N = n² as
-"""
-from math import sqrt
-from math import floor, ceil
-
-def decompo(n):
-    if n == 0:
-        return None
-    if n == 1:
-        return [1]
-    # if n == 5:
-    max_number = int(floor(sqrt(n)))
-    if n == max_number ** 2:
-        max_number -= 1
-    while (n - max_number**2) > 0:
-        delta = n - max_number ** 2
-        if max_number < 2:
-            return None
-        if decompo(delta) is None :
-            max_number -= 1
-        else:
-            if ceil(sqrt(delta)) == int(floor(sqrt(delta))):
-                return [int(floor(sqrt(delta))), max_number]
-            else:
-                if max_number not in decompo(delta):
-                    return sorted(decompo(delta) + [max_number])
-                else:
-                    #continue
-                    max_number -= 1
-
-    return None
-
-# print(decompo(104))
+def decompose_aux(nb, rac):
+    if (nb == 0):
+        return []
+    i = rac
+    l = None
+    while (i >= int(math.sqrt(nb / 2.0)) + 1):
+        diff = nb - i * i
+        rac = int(math.sqrt(diff))
+        l = decompose_aux(diff, rac);
+        if l != None:
+            l.append(i)
+            return l
+        i -= 1
+    return l
 
 def decompose(n):
-    return decompo(n*n)
+    l = decompose_aux(n * n, int(math.sqrt(n * n - 1)))
+    if l != None:
+        return l
+    else:
+        return None
 
 
-# print(decompose(5))
-# print(decompo(124))
-# print(decompo(72))
-print(decompo(38))
-#  7532  4 + 9 + 25 + 49
-# print(decompose(44))
-# print( 1936 - 43* 43)
+
+from math import sqrt
+
+def doDecompose(n2, prev = None):
+    for i in xrange(int(sqrt(n2)) - (0 if prev else 1), 0, -1):
+        if not prev or i < prev:
+            d = n2 - i ** 2
+            if d:
+                next = doDecompose(d, i)
+                if next:
+                    return next + [i, ]
+            else:
+                return [i, ]
+    return None
+
+def decompose(n):
+    return doDecompose(n ** 2)
+
+
+print(decompose(16))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def oye():
+    print('o'*79)
+
+
+
+
+
+
+
+
+
+from math import sqrt, ceil
+
+def doDecompose():
+    if int(sqrt(n2)) == ceil
+
+
+oye()
+
+def sqInRect(lng, wdth):
+    if lng == wdth:
+        return None
