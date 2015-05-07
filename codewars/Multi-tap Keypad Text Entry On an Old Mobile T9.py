a13d7268b2aba2390273f2bb791e48fff79b19ea
staticor <staticor0828@gmail.com>
2015-05-07 15:10:14 +0800

    目录重建 去除存疑目录.

---
+++ codewars/Multi-tap Keypad Text Entry On an Old Mobile T9.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''
+Created on May , 2015
+@author: stevey
+'''
+
+BUTTONS = [ '1',   'abc2',  'def3',
+          'ghi4',  'jkl5',  'mno6',
+          'pqrs7', 'tuv8', 'wxyz9',
+            '*',   ' 0',    '#'   ]
+def presses(phrase):
+    return sum(1 + button.find(c) for c in phrase.lower() for button in BUTTONS if c in button)
