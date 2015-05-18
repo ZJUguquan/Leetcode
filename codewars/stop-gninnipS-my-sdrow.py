e2e99385ab072a58f1abd3b777bce14b345d2829
staticor <staticor0828@gmail.com>
2015-05-18 23:48:55 +0800

    not ac. 词反转.

--- codewars/stop-gninnipS-my-sdrow.py
+++ codewars/stop-gninnipS-my-sdrow.py
@@ -1,7 +1,22 @@
 
 
+import re
+
 
 def spin_words(sentence):
-    import re
-    rev = [len(x) >= 5  and x[::-1] or x for x in re.split('\s', sentence)]
-    return ''.join(rev)
+
+    fields = re.split('(\s)', sentence)
+    res = []
+    fields = [ele for ele in fields if len(ele) > 0]
+    for idx, ele in enumerate(fields):
+        if len(ele) >= 5:
+            res.append(ele[::-1])
+        elif ele != ' ' or (ele == ' ' and fields[idx - 1] == ' '):
+            res.append(ele)
+        continue
+
+    return ''.join(res)
+
+
+sentence = 'Hey  fellow  warriors'
+print spin_words(sentence)
