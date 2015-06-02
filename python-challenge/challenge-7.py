179f70ba3404e64bd27455429f0dbfbdf0bb3c35
staticor <staticor0828@gmail.com>
2015-06-02 10:09:22 +0800

    0601.

---
+++ python-challenge/challenge-7.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+
+url = 'http://www.pythonchallenge.com/pc/def/oxygen.html'
+
+from urllib import urlopen
+from PIL import Image, ImageDraw
+
+
+if __name__ == '__main__':
+
+    img = Image.open("oxygen.png")
+
+    # mode of the picture
+    print img.mode  # RGBA
+
+    w, h = img.size
+    print w, h  # 629 95
+    row = [img.getpixel((x, 45)) for x in range(0, w, 7)]
+    rs = [r for r, g, b, a in row if r == g == b]
+
+    print rs
+
+
