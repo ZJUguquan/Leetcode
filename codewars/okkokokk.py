8c40c598ea2b88e9eb0085f7efdc8391f1c7f9b3
staticor <staticor0828@gmail.com>
2015-07-27 18:37:58 +0800

    lala

---
+++ codewars/okkokokk.py
@@ -0,0 +1,10 @@
+
+
+# okkOokOo('Ok, Ook, Ooo!')  # -> 'H'
+# okkOokOo('Ok, Ook, Ooo?  Okk, Ook, Ok?  Okk, Okk, Oo?  Okk, Okk, Oo?  Okk, Okkkk!')  # -> 'Hello'
+# okkOokOo('Ok, Ok, Okkk?  Okk, Okkkk?  Okkk, Ook, O?  Okk, Okk, Oo?  Okk, Ook, Oo?  Ook, Ooook!')  # -> 'World!'
+
+
+print ord('H')
+
+print ord('e')
